73d597083c9138ef91c7da5d70905da532116526
kkiyama117 <k.kiyama117@gmail.com>
2019-10-08 17:25:31 +0900

    Add controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# User Controller without Devise
+class UsersController < ApplicationController
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+      passwords: 'users/passwords',
+      omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
+  # user root
+  get 'users', action: :show, controller: :users, as: :user_root
 end
