da5118cc77fb45a6bfc307d05500f6c5c63befc1
kkiyama117 <k.kiyama117@gmail.com>
2019-10-09 12:11:39 +0900

    Add Ability
    for pundit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_080117) do
+ActiveRecord::Schema.define(version: 2019_10_09_031046) do
+
+  create_table "abilities", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "o_auths", force: :cascade do |t|
     t.integer "user_id", null: false
